70093e5fe6638d01ecdeeeb360472550707fc022
Pycomet <alfredemmanuelinyang@gmail.com>
2022-11-18 22:40:49 +0100

    feat(FE-25): added a test about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // Only Page Components Rendered Here
 import IndexPage from './pages/index';
 import ErrorPage from './pages/error';
+import AboutPage from './pages/about';
 
 // Define Page Routes
 const router = createBrowserRouter([
     {
         path: '/',
         element: <IndexPage />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: '/about',
+        element: <AboutPage />,
         errorElement: <ErrorPage />
     }
 ]);
