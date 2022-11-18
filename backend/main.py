75ba65f2aef6f18def070e9b7c4966644e7eead0
Osilaja78 <osilajaabdulhameed@gmail.com>
2022-11-18 13:23:47 +0100

    started working on the company category endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
 import uvicorn
 import os
+
+# from .api.database.database import get_db
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -12,6 +15,12 @@ async def get_root():
         "url": "http://localhost:8000/docs"
     }
 
+
+@app.get('/company/ranks/{category}')
+# async def get_company_catagory(request: Request, category: str, db: Session = Depends(get_db)):
+async def get_company_catagory(request: Request, category: str):
+    # companies = db.query(models.Company).filter(models.Company.category == category)
+    return {"category": category}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, reload=True)
