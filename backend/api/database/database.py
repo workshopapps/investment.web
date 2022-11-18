61a9c69b05817ea6a8703b56177c4505c89a2dc2
Conradgabe <gisuekebho5880@gmail.com>
2022-11-18 08:55:08 +0000

    commented out the python-decouple packages and switched to python-dotenv to get the environment variable

--- backend/api/database/database.py
+++ backend/api/database/database.py
@@ -1,13 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from decouple import config
+# from decouple import config
 
-DATABASE_NAME = config('DATABASE_NAME')
-DATABASE_HOST = config('DATABASE_HOST')
-DATABASE_PORT = config('DATABASE_PORT')
-DATABASE_USER = config('DATABASE_USER')
-DATABASE_PASSWORD = config('DATABASE_PASSWORD')
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
