7390372b4b881f1789309048c0053b07388f96fb
oedjoat <ogunfoworadolapo@gmail.com>
2022-11-19 17:13:52 +0100

    Refactored the code
    included the couter in main.py and returned the value of intervals on the company_metrics.py file, cleaned white spaces and removed unnecessary import modules

--- backend/api/routers/company_timeframe.py
+++ backend/api/routers/company_timeframe.py
@@ -1,9 +1,9 @@
+import datetime, time
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.crud import base
 from api.crud.base import get_db
 from api.models import models
-from datetime import datetime
 
 router = APIRouter()
 
@@ -13,18 +13,16 @@ router = APIRouter()
 # Returns a url in the format curl --location -g --request GET '{{baseUrl}}/company/{id}/?startDateTimestamp=1668255638&endDateTimestamp=1668342483'
 async def get_company_timeframe(id: str, startDateTimestamp: int, endDateTimestamp: int, db: Session = Depends(get_db)):
 
+    company = base.get_company(db, id=id)
     """
     This gets the value of a company within a specified interval
     """
-    startDateTimestamp = datetime()
-    endDateTimestamp = datetime()
-    
-    interval = db.query(models.Ranking).filter(models.Ranking.created_at >= startDateTimestamp, models.Ranking.created_at <= endDateTimestamp)
+    interval = db.query(models.Ranking).filter(models.Ranking.created_at >= startDateTimestamp.datetime.timestamp(), models.Ranking.created_at <= endDateTimestamp.time.time())
 
     interval.json() # convert the data to json
 
     if interval is None:
-        raise HTTPException(status_code=404, detail="Company info not available")
+        raise HTTPException(status_code=404, detail="Company info not available for this intervals")
 
     return {'interval': interval}
     
