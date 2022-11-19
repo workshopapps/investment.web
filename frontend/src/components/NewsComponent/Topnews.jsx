71cdefd495280a960e4ddf2a39209c495cbca469
Romeoscript <romeobourne211@gmail.com>
2022-11-19 11:02:47 +0100

    update:updated the data into a json file

--- frontend/src/components/NewsComponent/Topnews.jsx
+++ frontend/src/components/NewsComponent/Topnews.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../../Assets/scss/topnews.scss';
 import { BsChevronRight } from 'react-icons/bs';
-import newimage from '../../Assets/images/Rectangle 4745.png';
+// import newimage from '../../Assets/images/Rectangle 4745.png';
+import Picks from './Picks';
+import { newFeed } from '../reusabledata';
 
 const Topnews = () => {
     return (
@@ -10,102 +12,26 @@ const Topnews = () => {
                 <span className="icon_span">
                     Top stories <BsChevronRight />
                 </span>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
+                {newFeed.map((data) => {
+                    return (
+                        <div className="top__news-first" key={data.id}>
+                            <aside>
+                                <p>
+                                    Reuters <span>{data.time}</span>
+                                </p>
+                                <h3>{data.text}</h3>
+                            </aside>
+                            <aside className="image__heith">
+                                <img src={data.image} alt="" />
+                            </aside>
+                        </div>
+                    );
+                })}
+
                 <div className="top__news-second"></div>
             </div>
 
-            <div className="topNews__second">
-                <span className="icon_span">
-                    Picks for you <BsChevronRight />
-                </span>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-                <div className="top__news-first">
-                    <aside>
-                        <p>
-                            Reuters <span>6 minuts ago</span>
-                        </p>
-                        <h3>5 Value Stocks to Buy Before They Pop Off</h3>
-                    </aside>
-                    <aside>
-                        <img src={newimage} alt="" />
-                    </aside>
-                </div>
-            </div>
+            <Picks />
         </section>
     );
 };
--- frontend/src/pages/News/NewsPage.jsx
+++ frontend/src/pages/News/NewsPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import Topnews from '../../components/NewsComponent/Topnews';
 import '../../Assets/scss/newspage.scss';
+import Localmarket from '../../components/NewsComponent/Localmarket';
+import Worldmarket from '../../components/NewsComponent/Worldmarket';
+import Longcap from '../../components/NewsComponent/Longcap';
+import Midcap from '../../components/NewsComponent/Midcap';
+import Smallcap from '../../components/NewsComponent/Smallcap';
+import Industry from '../../components/NewsComponent/Industry';
 
 const NewsPage = () => {
     return (
@@ -8,6 +14,14 @@ const NewsPage = () => {
             <h1>Today&apos;s financial news</h1>
             <div className="financialflex">
                 <Topnews />
+                <div className="main__body">
+                    <Localmarket />
+                    <Worldmarket />
+                    <Longcap />
+                    <Midcap />
+                    <Smallcap />
+                    <Industry />
+                </div>
             </div>
         </div>
     );
