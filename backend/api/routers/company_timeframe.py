e721370c1f35fdc4b392ad1def5f4f062acd81a7
oedjoat <ogunfoworadolapo@gmail.com>
2022-11-19 20:11:11 +0100

    removed unnecessary values and imports

--- backend/api/routers/company_timeframe.py
+++ backend/api/routers/company_timeframe.py
@@ -1,4 +1,3 @@
-import datetime, time
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from api.crud import base
@@ -13,17 +12,16 @@ router = APIRouter()
 # Returns a url in the format curl --location -g --request GET '{{baseUrl}}/company/{id}/?startDateTimestamp=1668255638&endDateTimestamp=1668342483'
 async def get_company_timeframe(id: str, startDateTimestamp: int, endDateTimestamp: int, db: Session = Depends(get_db)):
 
-    company = base.get_company(db, id=id)
     """
     This gets the value of a company within a specified interval
     """
-    interval = db.query(models.Ranking).filter(models.Ranking.created_at >= startDateTimestamp.datetime.timestamp(), models.Ranking.created_at <= endDateTimestamp.time.time())
+    interval = db.query(models.Ranking).filter(models.Ranking.created_at >= startDateTimestamp, models.Ranking.created_at <= endDateTimestamp)
+    
+    # convert the data to json
 
-    interval.json() # convert the data to json
+    if interval:
+        return {'interval': interval}
+    raise HTTPException(status_code=404, detail="Company info not available for this intervals")
 
-    if interval is None:
-        raise HTTPException(status_code=404, detail="Company info not available for this intervals")
-
-    return {'interval': interval}
     
 
