af1c884a4d1120bfd266b8b481a1a3c0099f845f
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-11-19 17:40:13 +0100

    initial data gathering implementation

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
 import uvicorn
 
 from api.models import models
@@ -6,6 +6,8 @@ from api.models import models
 from api.database import database
 from api.database.database import engine
 from api.routers import company_metrics, company_category
+from api.scripts.data_gathering import pick_four_random_companies
+from fastapi_utils.tasks import repeat_every
 
 database.Base.metadata.create_all(bind=engine)
 
@@ -14,6 +16,14 @@ app = FastAPI()
 app.include_router(company_metrics.router)
 app.include_router(company_category.router)
 
+async def test():
+    await pick_four_random_companies()
+
+@app.on_event("startup")
+@repeat_every(seconds=15)
+async def run_cron():
+    print('blah blah')
+    await test()
 
 @app.get('/')
 async def get_root():
