2b6934d3bad0702bb6c1aabf8f3db5e68c0b778d
Conradgabe <gisuekebho5880@gmail.com>
2022-11-19 00:10:46 +0000

    fixed model field bugs

--- backend/api/models/models.py
+++ backend/api/models/models.py
@@ -14,7 +14,7 @@ class Company(Base):
     location = Column(String(100))
     description = Column(String(1000))
     sector = Column(String(64), ForeignKey("sectors.sector_id"))
-    category = Column(String(64), ForeignKey("categories.category_id", on_delete="CASCADE"))
+    category = Column(String(64), ForeignKey("categories.category_id"))
     ticker = Column(String(64), ForeignKey("tickers.ticker_id"))
 
     sect_value = relationship("Sector", back_populates="sect")
@@ -28,7 +28,7 @@ class StockPrice(Base):
     __tablename__ = "stock_prices"
 
     stock_price_id = Column(String(64), primary_key=True, index=True, default=uuid4())
-    company = Column(String(64), ForeignKey("company.company_id", on_delete="CASCADE"))
+    company = Column(String(64), ForeignKey("company.company_id"))
     market_cap = Column(Float)
     stock_price = Column(Float)
     date = Column(DateTime(timezone=True), server_default=func.now())
@@ -47,7 +47,7 @@ class StockPrice(Base):
 class Ranking(Base):
     __tablename__ = 'rankings'
     ranking_id = Column(String(64), primary_key=True, index=True, default=uuid4())
-    company = Column(String(64), ForeignKey("company.company_id", on_delete="CASCADE"))
+    company = Column(String(64), ForeignKey("company.company_id"))
     score = Column(Float)
     methodology = Column(String(1000))
     created_at = Column(DateTime(timezone=True), server_default=func.now() )
@@ -66,10 +66,10 @@ class Ticker(Base):
     __tablename__ = "tickers"
     
     ticker_id = Column(String(64), primary_key=True, index=True, default=uuid4())
-    symbol = Column(String(10), max_length=20)
-    exchange_name = Column(String(30), max_length=200)
-    exchange_symbol = Column(String(10), max_length=200)
-    exchange_website = Column(String(30), max_length=200)
+    symbol = Column(String(10))
+    exchange_name = Column(String(30))
+    exchange_symbol = Column(String(10))
+    exchange_website = Column(String(30))
     isin = Column(Integer)
 
     tick = relationship("Company", back_populates="ticker_value")
@@ -79,7 +79,7 @@ class Financial(Base):
     __tablename__ = "financials"
 
     financial_id = Column(String(64), primary_key=True, index=True, default=uuid4())
-    company = Column(String(64), ForeignKey("company.company_id", on_delete="CASCADE"))
+    company = Column(String(64), ForeignKey("company.company_id"))
     date = Column(DateTime(timezone=True), server_default=func.now())
     equity = Column(Float)
     dividend_per_stock = Column(Float)
@@ -100,6 +100,6 @@ class Category(Base):
 
     category_id = Column(String(64), primary_key=True, index=True, default=uuid4())
     market_cap = Column(Float)
-    name = Column(String(30), max_length=100)
+    name = Column(String(30))
 
     cat = relationship("Company", back_populates="cat_value")
