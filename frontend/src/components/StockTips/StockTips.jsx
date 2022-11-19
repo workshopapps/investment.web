36a391af5abacd0622c2bcc523551a5efcc1d322
Azeez-Ismaila <azeezthegenius@gmail.com>
2022-11-19 14:40:46 +0100

    completed stocktips-page 80%

--- frontend/src/components/StockTips/StockTips.jsx
+++ frontend/src/components/StockTips/StockTips.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-// import charles from '../../assets/Stock/images/Pexels Photo by Charles Parker.png';
+import charles from '../../assets/Stock/images/Pexels Photo by Charles Parker.png';
 import image1 from '../../assets/Stock/images/Rectangle 35.png';
 import image2 from '../../assets/Stock/images/Rectangle 36.png';
 import image3 from '../../assets/Stock/images/Rectangle 37.png';
@@ -70,136 +70,135 @@ const StockTips = () => {
     ];
     return (
         <div>
-            <div
-                style={{
-                    // backgroundImage: `url(${charles})`
-                    backgroundImage:
-                        "url('../../assets/Stock/images/Pexels Photo by Charles Parker.png')"
-                }}
-                className="p-6 text-center h-full">
-                <h2>MyStockPlug</h2>
-                <h1>STOCK TIPS FOR ABSOLUTE BEGINNERS</h1>
-            </div>
-            <div className="p-3 flex flex-col gap-6">
-                <h1 className="text-3xl">Featured</h1>
-                <div className="grid grid-cols-6 gap-6 md:grid-cols-12   ">
-                    {featured.map((feature) => {
-                        return (
-                            <div
-                                key={feature.image}
-                                className="col-span-3 md:col-span-4 grid gap-1 ">
-                                <img src={feature.image} alt="" className="w-full " />
-                                <div>Stock tips Update</div>
-                                <a href="http://" className="font-bold text-xs">
-                                    {feature.description}
-                                </a>
-                                <div className=" text-xs font-light">16/11/2022 - 2MINS READ</div>
-                            </div>
-                        );
-                    })}
+            <div id="charles" className="bg-gray-500">
+                <div
+                    style={{
+                        // backgroundImage: `url(${charles})`
+                        backgroundImage: `url(require${charles})`
+                    }}
+                    className="p-6 text-center text-xl text-white font-bold">
+                    <h2>MyStockPlug</h2>
+                    <h1>STOCK TIPS FOR ABSOLUTE BEGINNERS</h1>
                 </div>
-                <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4 px-6 rounded-lg">
-                    Load More
-                    <img src={arrow} alt="" />
-                </button>
             </div>
-            <div className="p-3 flex flex-col gap-6">
-                <h1 className="text-3xl">Stock Tips Guide</h1>
-                <div className="grid grid-cols-6 md:grid-cols-12 gap-6">
-                    {StockTips.map((Stock) => {
-                        return (
-                            <div key={Stock.image} className="col-span-3 md:col-span-4 grid gap-1">
-                                <img src={Stock.image} alt="" className="w-full " />
-                                <div>Stock tips Update</div>
-                                <a href="http://" className="font-bold text-xs">
-                                    {Stock.description}
-                                </a>
-                                <div className=" text-xs font-light">16/11/2022 - 2MINS READ</div>
-                            </div>
-                        );
-                    })}
-                </div>
-                <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4  px-6 rounded-lg ">
-                    Load More
-                    <img src={arrow} alt="" />
-                </button>
-            </div>
-            <div className="p-3 flex flex-col gap-6 ">
-                <h1 className="text-3xl">Trending Stock Tips Article</h1>
-                <div className="grid gap-3 md:grid-cols-6">
-                    {TrendingStocks.map((Trend) => {
-                        return (
-                            <div
-                                key={Trend.image}
-                                className="col-span-3 flex gap-6 md:grid md:grid-cols-6 md:gap-8  h-full">
-                                <img
-                                    src={Trend.image}
-                                    alt=""
-                                    className="w-full rounded-lg md:h-full  "
-                                />
-                                <div className="flex flex-col md:col-span-3 gap-1 md:gap-0">
-                                    <h3 className=" text-green-600">Ade Pete</h3>
-                                    <h1>Trading Sally Holdings</h1>
-                                    <p className="text-xs">
-                                        Even with pressure on sales and earnings, stock, which is
-                                        moderately shorted, Even with pressure on sales shorted...
-                                    </p>
-                                    <a href="http://" target="_blank" rel="noopener noreferrer">
-                                        Click To Read
+            <div className="md:p-12">
+                <div className="p-3 flex flex-col gap-6">
+                    <h1 className="text-3xl">Featured</h1>
+                    <div className="grid grid-cols-6 gap-6 md:grid-cols-12   ">
+                        {featured.map((feature) => {
+                            return (
+                                <div
+                                    key={feature.image}
+                                    className="col-span-3 md:col-span-4 grid gap-1">
+                                    <img src={feature.image} alt="" className="w-full " />
+                                    <div>Stock tips Update</div>
+                                    <a href="http://" className="font-bold text-xs">
+                                        {feature.description}
                                     </a>
+                                    <div className=" text-xs font-light">
+                                        16/11/2022 - 2MINS READ
+                                    </div>
                                 </div>
-                            </div>
-                        );
-                    })}
-                </div>
-                <button className="flex items-center my-8 gap-2 mx-auto bg-white border-2 border-green-600 text-green-600 p-4 px-6 rounded-lg ">
-                    Load More
-                    <img src={arrow} alt="" className=" text-green-600 " />
-                </button>
-            </div>
-            {/* <div className="flex flex-col md:grid md:grid-cols-6 gap-2">
-                <img src={ManSpeaker} alt="" className="md:col-span-3 md:order-2" />
-                <div className="p-8 -mt-32 items-center flex flex-col gap-6 md:col-span-3 md:my-auto">
-                    <h1 className="text-2xl">Get The Best Stock Investment Advice Anywhere</h1>
-                    <p className="font-bold text-sm whitespace-pre-wrap w-screen px-3 text-center">
-                        On MyStockPlug, gain access to thousands of stocks and point precision
-                        advice on the best stocks to buy
-                    </p>
+                            );
+                        })}
+                    </div>
                     <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4 px-6 rounded-lg">
-                        Start Investing
+                        Load More
                         <img src={arrow} alt="" />
                     </button>
                 </div>
-            </div> */}
-            <div className="flex flex-col md:grid md:grid-cols-6 gap-2 md:mt-24 bg-gray-300 w-auto">
-                <img src={ManSpeaker} alt="" className="md:col-span-3 md:order-2 md:mx-auto" />
-                <div className="p-8 -mt-32 items-center md:justify-start md:m-auto  flex flex-col gap-6 md:col-span-3 md:my-auto">
-                    <h1 className="text-2xl md:text-6xl font-bold">
-                        Get The Best Stock Investment Advice Anywhere
-                    </h1>
-                    <p className="font-bold text-sm md:text-2xl md:text-left whitespace-pre-wrap w-screen md:w-auto px-3 text-center">
-                        On MyStockPlug, gain access to thousands of stocks and point precision
-                        advice on the best stocks to buy
+                <div className="p-3 flex flex-col gap-6">
+                    <h1 className="text-3xl">Stock Tips Guide</h1>
+                    <div className="grid grid-cols-6 md:grid-cols-12 gap-6">
+                        {StockTips.map((Stock) => {
+                            return (
+                                <div
+                                    key={Stock.image}
+                                    className="col-span-3 md:col-span-4 grid gap-1">
+                                    <img src={Stock.image} alt="" className="w-full " />
+                                    <div>Stock tips Update</div>
+                                    <a href="http://" className="font-bold text-xs">
+                                        {Stock.description}
+                                    </a>
+                                    <div className=" text-xs font-light">
+                                        16/11/2022 - 2MINS READ
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4  px-6 rounded-lg ">
+                        Load More
+                        <img src={arrow} alt="" />
+                    </button>
+                </div>
+                <div className="p-3 flex flex-col gap-6">
+                    <h1 className="text-3xl">Trending Stock Tips Article</h1>
+                    <div className="grid justify-center gap-10 md:grid-cols-6">
+                        {TrendingStocks.map((Trend) => {
+                            return (
+                                <div key={Trend.image} className="col-span-3 flex gap-4">
+                                    <img
+                                        src={Trend.image}
+                                        alt=""
+                                        className="w-auto rounded-lg md:h-auto  "
+                                    />
+                                    <div className="flex flex-col my-auto md:col-span-3 gap-1 md:gap-2 md:gap-0 font-bold">
+                                        <h3 className=" text-green-600">Ade Pete</h3>
+                                        <h1 className="  ">Trading Sally Holdings</h1>
+                                        <p className="text-xs">
+                                            Even with pressure on sales and earnings, stock, which
+                                            is moderately shorted, Even with pressure on sales
+                                            shorted...
+                                        </p>
+                                        <a href="http://" target="_blank" rel="noopener noreferrer">
+                                            Click To Read
+                                        </a>
+                                    </div>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <button className="flex items-center my-8 gap-2 mx-auto bg-white border-2 border-green-600 text-green-600 p-4 px-6 rounded-lg ">
+                        Load More
+                        <img src={arrow} alt="" className=" text-green-600 " />
+                    </button>
+                </div>
+                <div className="flex flex-col md:grid md:grid-cols-6 gap-2 md:mt-24">
+                    <img
+                        src={ManSpeaker}
+                        alt=""
+                        className="md:col-span-3 md:order-2 md:scale-150 md:mx-auto"
+                    />
+                    <div className="p-8 -mt-32 items-center md:justify-start md:m-auto  flex flex-col gap-6 md:col-span-3 md:my-auto">
+                        <h1 className="text-2xl md:text-6xl font-bold">
+                            Get The Best Stock Investment Advice Anywhere
+                        </h1>
+                        <p className="font-bold text-sm md:text-2xl md:text-left whitespace-pre-wrap w-screen md:w-auto px-3 text-center">
+                            On MyStockPlug, gain access to thousands of stocks and point precision
+                            advice on the best stocks to buy
+                        </p>
+                        <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4 px-6 rounded-lg">
+                            Start Investing
+                            <img src={arrow} alt="" />
+                        </button>
+                    </div>
+                </div>
+                <div className="flex w-full items-center justify-center">
+                    <h1>Subscribe for latest Stock Tips Update</h1>
+                    <img src={bell} alt="" className=" float-right" />
+                </div>
+                <div className="flex flex-col md:flex-row p-2 gap-2 justify-between md:gap-12 md:text-xl text-xs bg-gray-300 w-11/12 md:w-full h-3/4 mx-auto rounded-lg">
+                    <p className="text-center p-2 md:px-6">
+                        Never miss out on anything about stock, by joining our Mailing List
                     </p>
-                    <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4 px-6 rounded-lg">
-                        Start Investing
-                        <img src={arrow} alt="" />
-                    </button>
-                </div>
-            </div>
-            <div className="flex w-full items-center justify-center">
-                <h1>Subscribe for latest Stock Tips Update</h1>
-                <img src={bell} alt="" className=" float-right" />
-            </div>
-            <div className="flex gap-4 items-center">
-                <div>Never miss out on anything about stock, by joining our Mailing List</div>
-                <div className="">
-                    {' '}
-                    <input type="email" name="" id="" placeholder="Enter your email" />
-                    <button className="flex items-center gap-2 mx-auto bg-green-600 text-white p-4 ">
-                        Subscribe
-                        <img src={arrow} alt="" />
-                    </button>
+                    <span className="flex justify-between bg-white p-2 rounded-lg w-auto md:w-2/4 mx-auto">
+                        <input type="email" name="" id="" placeholder="Enter your email" />
+                        <button className="flex items-center p-2 gap-2 bg-green-600 text-white  rounded-lg">
+                            Start Investing
+                            <img src={arrow} alt="" />
+                        </button>
+                    </span>
                 </div>
             </div>
         </div>
