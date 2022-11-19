94bf194bf494a6a44c33275146f4af1481a9bd2a
raeeceip <chiboguchisomu@gmail.com>
2022-11-19 10:34:58 +0100

    FEAT: ading password reset files

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,13 @@ from fastapi import FastAPI
 import uvicorn
 from api.models import models
 from api.database.database import engine
-from api.routes import rankings
+from api.routes import rankings, password_reset
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 app.include_router(rankings.router)
-
+app.include_router(password_reset.router)
 
 @app.get('/')
 async def get_root():
