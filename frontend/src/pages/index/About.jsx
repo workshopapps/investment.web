f99ef2f8c38e70c6c2ff64b3b69a1fae3a259728
dread557 <dadaolawunmi09@gmail.com>
2022-11-19 16:07:48 +0100

    feat(FE-15): implemented all about components

--- frontend/src/pages/index/About.jsx
+++ frontend/src/pages/index/About.jsx
@@ -1,10 +1,21 @@
 /* eslint-disable prettier/prettier */
-import React from 'react'
+import React from 'react';
+import AboutNav from '../../components/about/AboutNav';
+import Goals from '../../components/about/Goals';
+import HeroSection from '../../components/about/HeroSection';
+import Section from '../../components/about/MiddleSection';
+import OurTeam from '../../components/about/OurTeam';
 
-const About = () => {
+const AboutPage = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="bg-[#fafaff] text-[#455A64]">
+      <HeroSection />
+      <AboutNav />
+      <Section />
+      <Goals />
+      <OurTeam />
+    </div>
+  );
+};
 
-export default About
+export default AboutPage;
