05db08ea44ac911dc7f313ab3bb297910ded4035
Azeez-Ismaila <azeezthegenius@gmail.com>
2022-11-19 12:36:06 +0100

    feat(FE-12):added stock tips page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,18 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 // Only Page Components Rendered Here
 import IndexPage from './pages/index';
 import ErrorPage from './pages/error';
+import { StockPage } from './pages/stock';
 
 // Define Page Routes
 const router = createBrowserRouter([
     {
         path: '/',
         element: <IndexPage />,
+        errorElement: <ErrorPage />
+    },
+    {
+        path: '/StockTips',
+        element: <StockPage />,
         errorElement: <ErrorPage />
     }
 ]);
