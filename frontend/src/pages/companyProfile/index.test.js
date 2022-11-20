44838cf617267f0d82c5094a514dd5b9afd4b67e
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-11-20 19:42:58 +0100

    fix companyProfile test err

--- frontend/src/pages/companyProfile/index.test.js
+++ frontend/src/pages/companyProfile/index.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { createRoot } from 'react-dom/client';
 import CompanyProfile from './index';
 
-test('renders without crashing', () => {
-    render(<CompanyProfile />);
-    const linkElement = screen.getByText(/MyStockPlug/i);
-    expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+    const element = document.createElement('div');
+    const root = createRoot(element);
+    root.render(<CompanyProfile />);
 });
