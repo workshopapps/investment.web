c8730f3a4daa45f4ce03078f7d72a9007d0edb40
Pycomet <alfredemmanuelinyang@gmail.com>
2022-11-20 01:29:30 +0100

    feat(FE-10): done with landing page

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -42,7 +42,8 @@ const Footer = () => {
     return (
         <footer
             style={footerStyle}
-            className="lg:px-[100px] p-6 flex flex-col items-center justify-center w-full">
+            className="lg:px-[100px] p-6 flex flex-col items-center justify-center w-full"
+        >
             <div className="flex w-full flex-wrap justify-between items-start gap-8 md:border-b border-white pb-12 mb-8 md:mt-12">
                 <div className="w-60 pb-8 md:pb-0">
                     <img src={logo} alt="" />
--- frontend/src/components/landingPage/FAQSection/FAQSection.jsx
+++ frontend/src/components/landingPage/FAQSection/FAQSection.jsx
@@ -5,29 +5,6 @@ import DownIcon from '../../../assets/landingPage/icons/down.svg';
 import RightArrow from '../../../assets/landingPage/icons/arrow-right.svg';
 
 const FAQSection = () => {
-    const faqData = [
-        {
-            index: 0,
-            question: 'How do I do my stock analysis?',
-            answer: 'With our stock analysis, its as easy as ABC. All you have to do is to check our details and you can get information as regards the particular stock you need'
-        },
-        {
-            index: 1,
-            question: 'Can I buy multiple Stocks?',
-            answer: ''
-        },
-        {
-            index: 2,
-            question: 'I have no experience in stock, Is this for me?',
-            answer: ''
-        },
-        {
-            index: 3,
-            question: 'How do you make your own money as an institution?',
-            answer: ''
-        }
-    ];
-
     const [show, setShow] = React.useState(false);
 
     return (
@@ -38,36 +15,67 @@ const FAQSection = () => {
                 </span>
 
                 <div className="text-left w-full">
-                    {faqData.map((each) => {
-                        return (
-                            <>
-                                <div className="flex flex-row justify-between my-4 shadow-md">
-                                    <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
-                                        How do I do my stock analysis?
-                                    </span>
-                                    {show ? (
-                                        <span
-                                            className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
-                                            onClick={() => setShow(false)}>
-                                            <img src={UpIcon} alt="open" />
-                                        </span>
-                                    ) : (
-                                        <span
-                                            className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
-                                            onClick={() => setShow(true)}>
-                                            <img src={DownIcon} alt="open" />
-                                        </span>
-                                    )}
-                                </div>
+                    <div className="flex flex-row justify-between my-4 shadow-md">
+                        <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
+                            How do I do my stock analysis?
+                        </span>
+                        {show ? (
+                            <span
+                                className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
+                                onClick={() => setShow(false)}>
+                                <img src={UpIcon} alt="open" />
+                            </span>
+                        ) : (
+                            <span
+                                className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
+                                onClick={() => setShow(true)}>
+                                <img src={DownIcon} alt="open" />
+                            </span>
+                        )}
+                    </div>
 
-                                {show ? (
-                                    <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
-                                        With our stock analysis, its as easy as ABC. All you have to do is to check our details and you can get information as regards the particular stock you need
-                                    </span>
-                                ) : null}
-                            </>
-                        );
-                    })}
+                    {show ? (
+                        <div className="flex flex-row justify-between my-4 shadow-md">
+                            <span className="text-[#000718] text-sm md:text-xl max-w-[80%] tracking-[0.25px] align-middle m-4 font-light font-Hauora">
+                                With our stock analysis, its as easy as ABC. All you have to do is
+                                to check our details and you can get information as regards the
+                                particular stock you need
+                            </span>
+                        </div>
+                    ) : null}
+
+                    <div className="flex flex-row justify-between my-4 shadow-md">
+                        <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
+                            Can I buy multiple Stocks?
+                        </span>
+                        <span
+                            className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
+                            onClick={() => setShow(true)}>
+                            <img src={DownIcon} alt="open" />
+                        </span>
+                    </div>
+
+                    <div className="flex flex-row justify-between my-4 shadow-md">
+                        <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
+                            I have no experience in stock, Is this for me?
+                        </span>
+                        <span
+                            className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
+                            onClick={() => setShow(true)}>
+                            <img src={DownIcon} alt="open" />
+                        </span>
+                    </div>
+
+                    <div className="flex flex-row justify-between my-4 shadow-md">
+                        <span className="text-[#000718] text-md md:text-2xl tracking-[0.25px] align-middle m-4 font-bold font-Hauora">
+                            How do you make your own money as an institution?
+                        </span>
+                        <span
+                            className="text-[#000718] md:text-2xl md:pt-4 pt-2 tracking-[0.25px] align-middle m-4 font-bold font-Hauora"
+                            onClick={() => setShow(true)}>
+                            <img src={DownIcon} alt="open" />
+                        </span>
+                    </div>
                 </div>
 
                 <button className="flex rounded-[8px] text-[16px] lg:mx-[45%] mt-10 mx-[25%] justify-center content-center font-semibold bg-[#1BD47B] p-4 font-Hauora">
