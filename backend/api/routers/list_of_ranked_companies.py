b2413b5114b9a7c8d46b6a696c11df112d03ccd2
John Doe <seraho.nwoke@gmail.com>
2022-11-20 02:48:33 +0100

    feat(BE-10): added list of ranked companies endpoi

--- backend/api/routers/list_of_ranked_companies.py
+++ backend/api/routers/list_of_ranked_companies.py
@@ -1,14 +1,50 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+from dotenv import load_dotenv
+from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from api.models import models
 from api.crud.base import get_db
+from backend.api.models.models import Ranking
 
 router = APIRouter()
 
-@router.get('/company/ranking')
-async def get_list_of_ranked_companies(score: float, db: Session = Depends(get_db)):
-    companies_ranking = db.query(models.Ranking).filter(models.Ranking.id == score).all()
 
-    if companies_ranking:
-        return companies_ranking
-    raise HTTPException(status_code=404, detail="List of ranked companies unavailable")
+@router.get('/company/ranking')
+def get_list_of_ranked_companies():
+   db: Session = next(get_db())
+   # get companies
+   low_cap_category_id = os.getenv('LOW_MARKET_CAP_CATEGORY_ID')
+   companies: list = db.query(models.Company).filter(models.Company.category != low_cap_category_id).all()
+
+   # get latest rankings
+   rankings: list = []
+   for company in companies:
+       rank = db.query(Ranking).filter(Ranking.company == company.company_id)\
+           .order_by(Ranking.created_at.desc()).first()
+       if rank:
+           rankings.append(rank)
+
+   # sort rankings by score (descending)
+   def get_ranking_sort_key(inner_rank: Ranking):
+       return inner_rank.score
+   rankings.sort(key=get_ranking_sort_key, reverse=True)
+
+   # create the response list
+   response = []
+   for ranking in rankings:
+       comp: models.Company = ranking.comp_ranks
+       data = {
+           'company_id': comp.company_id,
+           'name': comp.name,
+           'sector': comp.sect_value,
+           'category': comp.cat_value,
+           'ticker_symbol': comp.ticker_value.symbol,
+           'exchange_platform': comp.ticker_value.exchange_name,
+           'current_ranking': {
+               'score': ranking.score,
+               'created_at': ranking.created_at
+           }
+       }
+       response.append(data)
+
+   return response
