faf565e7bf0d03f12c6ebd70ae6b611dde46bd11
oedjoat <ogunfoworadolapo@gmail.com>
2022-11-30 22:34:22 +0100

    added a function to send email to only subscribed users

--- backend/api/scripts/email_sending.py
+++ backend/api/scripts/email_sending.py
@@ -30,3 +30,31 @@ async def send_user_email(company_ranks):
     for e in emails:
         destinations.append(e[0])
     await send_email("Updated List of Ranked Companies", destinations, body)
+
+
+
+async def send_subscribedUser_email(stocks):
+    db: Session = next(get_db())
+
+    # get users emails
+    emails = db.query(models.User.email).join(models.Subscription).all()
+    
+    body = "<html> <body><h4>Top 10 Recommended Stocks</h4>"
+    body += "<table><thead><tr><th>Company Name</th><th>Company Rank</th><th>Position</th></tr></thead>"
+    body += "<tbody>"
+
+    position = 0
+    for recommended in stocks:
+        position +=1
+        body += f"<tr><td>{recommended['company']}</td>"
+        body += f"<td>{recommended['current_ranking']}</td><td>{position}</td></tr>"
+    
+    body += "</tbody></table></body></html>"
+
+    def map_emails(e):
+        return e[0]
+
+    destinations = []
+    for e in emails:
+        destinations.append(e[0])
+    await send_email("Recommended Stocks", destinations, body)
