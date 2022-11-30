658b0223146d957ad7ec90cf1aff5d70d54d30b0
Romeoscript <romeobourne211@gmail.com>
2022-11-30 23:12:29 +0100

    style:contact form styled

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -7,38 +7,40 @@ import ContactForm from './ContactForm';
 const Contact = () => {
     return (
         <PageLayout>
-            <section className="flex">
-                <div>
-                    <aside>
-                        <h1>Contact us</h1>
+            <section className="flex min-h-fit justify-around p-12 ">
+                <div className="basis-[40%] ">
+                    <aside className="mb-4">
+                        <h1 className="font-bold">Contact us</h1>
                         <p>Got any concerns you want us to sort out for you?</p>
                     </aside>
-                    <aside>
+                    <aside className="mb-4">
                         <h2>For general Enquiries</h2>
                         <p>support@yieldvest.com</p>
                     </aside>
-                    <aside>
-                        <h2>Mailing Address</h2>
+                    <aside className="mb-4">
+                        <h2 className="font-bold">Mailing Address</h2>
                         <p>
                             Plot 1b, industrial avenue, Apapa. Lagos +2348100123456
                             enquiries@yieldvest.com
                         </p>
                     </aside>
-                    <aside>
-                        <h2>For Media, Business development and partnerships:</h2>
+                    <aside className="mb-4">
+                        <h2 className="font-bold">
+                            For Media, Business development and partnerships:
+                        </h2>
                         <p>business@yieldvest.com</p>
                     </aside>
-                    <aside>
-                        <h2>Follow us on all our social pages</h2>
+                    <aside className="mb-4">
+                        <h2 className="font-bold">Follow us on all our social pages</h2>
                         <p>business@yieldvest.com</p>
                     </aside>
-                    <aside>
+                    <aside className="flex">
                         <BsFacebook />
-                        <AiFillInstagram />
+                        <AiFillInstagram className="mx-4" />
                         <BsTwitter />
                     </aside>
                 </div>
-                <div>
+                <div className="basis-[50%] ">
                     <ContactForm />
                 </div>
             </section>
--- frontend/src/pages/contact/ContactForm.jsx
+++ frontend/src/pages/contact/ContactForm.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
 const ContactForm = () => {
-    return <div>form</div>;
+    const FormStyle = {
+        background: '#000718',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        padding: '2rem'
+    };
+    return (
+        <div>
+            <form action="" style={FormStyle} className="rounded-lg">
+                <h2 className="text-left text-white">Send us a message</h2>
+                <input
+                    type="text"
+                    placeholder="Name"
+                    className="w-[100%] m-3 p-1 rounded-md outline-none  bg-transparent border-[1px] "
+                />
+                <input
+                    type="email"
+                    name=""
+                    id=""
+                    placeholder="Email"
+                    className="w-[100%] m-3 p-1 rounded-md outline-none bg-transparent border-[1px]  "
+                />
+                <textarea
+                    name=""
+                    id=""
+                    cols="30"
+                    rows="10"
+                    placeholder="Message"
+                    className="w-[100%] m-3 p-1 rounded-md outline-none bg-transparent border-[1px] "></textarea>
+                <button className="w-[100%] m-3 p-1 rounded-md outline-none bg-[green] text-white">
+                    send message
+                </button>
+            </form>
+        </div>
+    );
 };
 
 export default ContactForm;
