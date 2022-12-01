e779d3359c6d6e74e7578c589eebc2383f2a2678
Zrnited <kolamayh@gmail.com>
2022-12-01 15:19:51 +0100

    Updated the UI and removed unnecessary files

--- frontend/src/pages/how-it-works/Index.js
+++ frontend/src/pages/how-it-works/Index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import PageLayout from '../layout';
-import signUpImg from '../../assets/how-it-works/signUpImg.png';
-import setUpImg from '../../assets/how-it-works/setUpImg.png';
-import getImg from '../../assets/how-it-works/getImg.png';
+import signUpImg from '../../assets/how-it-works/signupimg.png';
+import setUpImg from '../../assets/how-it-works/setupimg.png';
+import getImg from '../../assets/how-it-works/getimg.png';
 import arrowRight from '../../assets/how-it-works/arrow-right.png';
-import arrow2 from '../../assets/how-it-works/arrow2.png';
+// import arrow2 from '../../assets/how-it-works/arrow2.png';
 import arrow from '../../assets/how-it-works/m-arr-1.png';
 import { Link } from 'react-router-dom';
 
@@ -51,17 +51,17 @@ const Index = () => {
                 </div>
 
                 {/* SMALLER SCREEN */}
-                <div className="p-10 flex flex-col gap-8 lg:hidden">
+                <div className="p-10 flex flex-col gap-8 lg:flex-row lg:items-start lg:justify-between">
                     {process?.map((item, index) => {
                         return (
                             <div key={index} className="flex flex-col justify-center gap-10">
                                 <div className="flex">
-                                    <p className="px-3 py-1 border border-black rounded-full">
+                                    <p className="px-3 py-1 border border-black rounded-full font-HauoraBold">
                                         {item?.id}
                                     </p>
                                 </div>
-                                <div className="flex flex-col items-center gap-2">
-                                    <img src={item?.image} className="w-64 sm:w-1/2 lg:w-64" />
+                                <div className="flex flex-col items-center gap-2 lg:w-210">
+                                    <img src={item?.image} className="w-64 sm:w-1/2 lg:w-full" />
                                     <h1 className="font-HauoraBold text-sm text-center sm:text-xl">
                                         {item?.headingText}
                                     </h1>
@@ -69,20 +69,19 @@ const Index = () => {
                                         {item?.subText}
                                     </p>
                                     {item?.subText2 && (
-                                        <p className="text-sm text-green-500 cursor-pointer hover:text-green-600 transition ease-in-out delay-100">
+                                        <p className="text-sm text-green-500 cursor-pointer hover:text-green-600 transition ease-in-out delay-100 lg:text-base">
                                             {item?.subText2}
                                         </p>
                                     )}
-                                    {item?.arrow && <img src={item?.arrow} className="mt-4" />}
                                 </div>
                             </div>
                         );
                     })}
 
-                    <div className="flex justify-center mt-6">
+                    <div className="flex justify-center mt-6 lg:hidden">
                         <Link
                             to={'/signup'}
-                            className="text-sm flex items-center gap-2 bg-green-400 py-3 px-6 rounded-md hover:bg-green-500 transition ease-in-out delay-100">
+                            className="text-sm flex items-center gap-2 bg-green-400 py-3 px-6 rounded-md hover:bg-green-500 transition ease-in-out delay-100 lg:hidden">
                             <p className="font-HauoraBold">Get Started</p>
                             <img src={arrowRight} />
                         </Link>
@@ -90,72 +89,10 @@ const Index = () => {
                 </div>
 
                 {/* LARGER SCREENS */}
-                <div className="hidden lg:flex flex-row justify-between py-10 px-4 xl:p-8">
-                    <div className="relative w-210">
-                        <div className="flex mb-12">
-                            <p className="px-3.5 py-1 border border-black rounded-full text-lg font-HauoraBold">
-                                1
-                            </p>
-                        </div>
-                        <div className="flex flex-col gap-2 items-center">
-                            <img src={signUpImg} width={'250px'} className="mb-5" />
-                            <h1 className="text-xl font-Hauora">Sign up for your free account</h1>
-                            <p className="font-Hauora text-sm text-center">
-                                Open a premium account for full functionality
-                            </p>
-                            <Link to={'/about'} className="font-Hauora text-green-500 text-sm">
-                                Learn more
-                            </Link>
-                        </div>
-                        <img
-                            src={arrow2}
-                            className="absolute bottom-36 lg:left-56 lg:z-10 lg:h-24 xl:h-28 xl:left-64"
-                        />
-                    </div>
-                    <div className="relative w-210">
-                        <div className="flex mb-12">
-                            <p className="px-3 py-1 border border-black rounded-full text-lg font-HauoraBold">
-                                2
-                            </p>
-                        </div>
-                        <div className="flex flex-col gap-2 items-center">
-                            <img src={setUpImg} width={'250px'} className="mb-5" />
-                            <h1 className="text-xl font-Hauora">Set up your account</h1>
-                            <p className="font-Hauora text-sm text-center">
-                                Create your profile and answer our short quiz on your risk appetite.
-                            </p>
-                            {/* <Link className="font-Hauora text-green-500 text-sm">Learn more</Link> */}
-                        </div>
-                        <img
-                            src={arrow2}
-                            className="absolute bottom-36 lg:left-56 lg:z-10 h-24 xl:h-28 xl:left-64"
-                        />
-                    </div>
-                    <div className="relative w-210">
-                        <div className="flex mb-12">
-                            <p className="px-3 py-1 border border-black rounded-full text-lg font-HauoraBold">
-                                3
-                            </p>
-                        </div>
-                        <div className="flex flex-col gap-2 items-center">
-                            <img src={getImg} width={'250px'} className="mb-5" />
-                            <h1 className="text-xl font-Hauora text-center">
-                                Get your custom-made recommendations
-                            </h1>
-                            <p className="font-Hauora text-sm text-center">
-                                From the short quiz, we understand your interests and curate stock
-                                options that are tailored to you.
-                            </p>
-                            {/* <Link className="font-Hauora text-green-500 text-sm">Learn more</Link> */}
-                        </div>
-                        {/* <img src={arrow2} className="absolute top-40 left-52" height={'40px'} /> */}
-                    </div>
-                </div>
-
-                <div className="hidden lg:flex justify-center mt-6 mb-10">
+                <div className="hidden lg:flex lg:justify-center lg:m-10">
                     <Link
                         to={'/signup'}
-                        className="text-sm flex items-center gap-2 bg-green-400 py-3 px-20 rounded-md hover:bg-green-500 transition ease-in-out delay-100">
+                        className="text-sm flex items-center gap-2 bg-green-400 py-3 px-12 rounded-md hover:bg-green-500 transition ease-in-out delay-100">
                         <p className="font-HauoraBold">Get Started</p>
                         <img src={arrowRight} />
                     </Link>
