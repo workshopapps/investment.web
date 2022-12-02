9b21fd9f808f54956bb1b61c5e09438f1b16efb3
Romeoscript <romeobourne211@gmail.com>
2022-12-02 14:46:51 +0100

    style:styled the help page

---
+++ frontend/src/pages/help/HeroSection.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import searchIcon from '../../assets/help/search.svg';
+const HeroSection = () => {
+    return (
+        
+          <div
+          data-testid="hero-section"
+          className='flex bg-[url("/src/assets/images/myhero.png")] relative bg-no-repeat bg-cover  items-center pl-5 md:pl-[50px] lg:pl-[100px] h-[160px] md:h-[360px]'>
+          <div className="flex flex-col text-white gap-[6px] absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] w-[90%]">
+          <h1 className="font-bold text-center sm:text-3xl md:text-4xl text-xl lg:text-[40px]  text-white lg:font-semibold mb-10">
+          The Yieldvest Help Center
+                </h1>
+                <p className='text-white'>How Can We Help You?</p>
+                <div className="flex items-center h-12 lg:h-14 border pl-3 lg:pl-4 focus-within:border-black border-[#A3AAB2] bg-white rounded">
+                    <input
+                        className="placeholder:text-xs h-6 md:sm:placeholder:text-sm lg:placeholder:text-base placeholder:font-normal placeholder:text-[#A3AAB2] w-full focus:outline-0 "
+                        type="text"
+                        placeholder="Ask Your Question"
+                    />
+                    <button className="w-12 lg:w-14 h-full flex items-center justify-center">
+                        <img src={searchIcon} className="w-6 h-6" alt="search" />
+                    </button>
+                </div>
+          </div>
+      </div>
+    );
+};
+
+export default HeroSection;
--- frontend/src/pages/help/index.jsx
+++ frontend/src/pages/help/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Faq from '../../components/Help/Faq';
-import searchIcon from '../../assets/help/search.svg';
+// import searchIcon from '../../assets/help/search.svg';
 import PageLayout from '../layout';
+import HeroSection from './HeroSection';
 
 function HelpPage() {
     const faqs = {
@@ -61,21 +62,8 @@ function HelpPage() {
     };
     return (
         <PageLayout>
+            <HeroSection />
             <div className="py-8 font-Hauora sm:py-12 md:py-14 lg:py-16  lg:pb-16 px-4 sm:px-10 md:px-14 lg:px-32 xl:px-[206px] flex flex-col w-full">
-                <h1 className="font-bold sm:text-3xl md:text-4xl text-xl lg:text-[40px]  lg:font-semibold mb-4  text-[#0A0B0D]">
-                    How Can We Help You?
-                </h1>
-                <div className="flex items-center h-12 lg:h-14 border pl-3 lg:pl-4 focus-within:border-black border-[#A3AAB2] rounded">
-                    <input
-                        className="placeholder:text-xs h-6 md:sm:placeholder:text-sm lg:placeholder:text-base placeholder:font-normal placeholder:text-[#A3AAB2] w-full focus:outline-0 "
-                        type="text"
-                        placeholder="Ask Your Question"
-                    />
-                    <button className="w-12 lg:w-14 h-full flex items-center justify-center">
-                        <img src={searchIcon} className="w-6 h-6" alt="search" />
-                    </button>
-                </div>
-
                 <div className="mt-6 sm:mt-8 lg:mt-10 text-[#0A0B0D]">
                     <h2 className="font-bold lg:text-semibold sm:text-lg md:text-xl lg:text-2xl text-base text-[#0A0B0D] mb-8">
                         Frequently Asked Questions
