e17110dee8137c37833cc21f0a24909fec3c9667
Romeoscript <romeobourne211@gmail.com>
2022-12-02 14:53:35 +0100

    refactor:final change on help page

--- frontend/src/pages/help/index.jsx
+++ frontend/src/pages/help/index.jsx
@@ -8,12 +8,12 @@ function HelpPage() {
     const faqs = {
         faq: [
             {
-                question: 'What is MyStockPlug used for?',
-                answer: 'It allows you to see the best performing stocks in the market to buy and top companies to invest in at the moment. MyStockPlug has a simple, straightforward user interface that shows real-time portfolio data, market value and growth potential of top companies and stocks.'
+                question: 'What is Yieldvest used for?',
+                answer: 'It allows you to see the best performing stocks in the market to buy and top companies to invest in at the moment. Yieldvest has a simple, straightforward user interface that shows real-time portfolio data, market value and growth potential of top companies and stocks.'
             },
             {
                 question: 'Can I trade stocks on this platform?',
-                answer: "No. MyStockPlug only provides information and suggestions on company's stocks to buy. After you must have known the stocks to buy, head over to the listed stock trading platform to buy and build your portfolio"
+                answer: "No. Yieldvest only provides information and suggestions on company's stocks to buy. After you must have known the stocks to buy, head over to the listed stock trading platform to buy and build your portfolio"
             },
             {
                 question: 'How can I know stocks that fit my investment goals?',
@@ -21,21 +21,21 @@ function HelpPage() {
             },
             {
                 question: 'How can I know more about the company to invest in?',
-                answer: 'On MyStockPlug, every company has their details and company portfolio listed, obtained from reliable sources.'
+                answer: 'On Yieldvest, every company has their details and company portfolio listed, obtained from reliable sources.'
             },
             {
                 question: 'I have no experience in stocks. Is this for me?',
-                answer: 'Yes, anyone interested in stocks can use it, even complete beginners. MyStockPlug is easy to use.'
+                answer: 'Yes, anyone interested in stocks can use it, even complete beginners. Yieldvest is easy to use.'
             },
             {
                 question: 'How can I access the platform?',
-                answer: 'MyStockPlug is available as a website and a desktop application. This means you can access it on your mobile, as you go. And also on any computer, whether laptop or desktop.',
+                answer: 'Yieldvest is available as a website and a desktop application. This means you can access it on your mobile, as you go. And also on any computer, whether laptop or desktop.',
                 link: 'Download for Desktop'
             }
         ],
         popular_questions: [
             {
-                question: 'Is MyStockPlug reliable?',
+                question: 'Is Yieldvest reliable?',
                 answer: 'You can be sure that this is a reliable service. Decisions are made based on credible algorithms and metrics.'
             },
             {
@@ -65,7 +65,7 @@ function HelpPage() {
             <HeroSection />
             <div className="py-8 font-Hauora sm:py-12 md:py-14 lg:py-16  lg:pb-16 px-4 sm:px-10 md:px-14 lg:px-32 xl:px-[206px] flex flex-col w-full">
                 <div className="mt-6 sm:mt-8 lg:mt-10 text-[#0A0B0D]">
-                    <h2 className="font-bold lg:text-semibold sm:text-lg md:text-xl lg:text-2xl text-base text-[#0A0B0D] mb-8">
+                    <h2 className="font-bold lg:text-semibold sm:text-lg md:text-xl lg:text-2xl text-base text-[#0B5934] mb-8">
                         Frequently Asked Questions
                     </h2>
                     <div className="w-full ">
@@ -81,7 +81,7 @@ function HelpPage() {
                 </div>
 
                 <div className="mt-6 sm:mt-8 lg:mt-10">
-                    <h2 className="font-bold lg:text-semibold sm:text-lg md:text-xl lg:text-2xl text-base text-[#0A0B0D] mb-8">
+                    <h2 className="font-bold lg:text-semibold sm:text-lg md:text-xl lg:text-2xl text-base text-[#0B5934] mb-8">
                         Popular Questions
                     </h2>
 
