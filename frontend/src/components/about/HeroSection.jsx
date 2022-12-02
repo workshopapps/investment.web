e4f6cc9e61719b06e8e34f78ae0b7bc7bb93a3f8
Romeoscript <romeobourne211@gmail.com>
2022-12-02 13:51:16 +0100

    style:changed the bg image for the about us page

--- frontend/src/components/about/HeroSection.jsx
+++ frontend/src/components/about/HeroSection.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 
 const HeroSection = () => {
     return (
-        <div
-            data-testid="hero-section"
-            className='flex bg-[url("/src/assets/about/hero.png")] bg-no-repeat bg-cover  items-center pl-5 md:pl-[50px] lg:pl-[100px] h-[160px] md:h-[360px]'>
-            <div className="flex flex-col text-white gap-[6px]">
-                <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold">About Us</h1>
-                <p className="text-xs md:text-2xl font-[500]">The Spark that ignites your dreams</p>
-            </div>
-        </div>
+        
+          <div
+          data-testid="hero-section"
+          className='flex bg-[url("/src/assets/images/hero.png")] relative bg-no-repeat bg-cover  items-center pl-5 md:pl-[50px] lg:pl-[100px] h-[160px] md:h-[360px]'>
+          <div className="flex flex-col text-white gap-[6px] absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+              <h1 className="text-3xl md:text-4xl lg:text-6xl font-bold text-center">About Us</h1>
+              <p className="text-xs md:text-2xl font-[500] text-center">The Spark that ignites your dreams</p>
+          </div>
+      </div>
     );
 };
 
