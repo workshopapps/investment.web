a9676c619d62a79abefec66429e2f2b1688730e8
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-12-03 23:31:28 +0100

    update

--- frontend/src/components/settings/ProfileSection.jsx
+++ frontend/src/components/settings/ProfileSection.jsx
@@ -4,15 +4,38 @@ import React, { useContext } from 'react';
 import AuthContext from '../../auth/AuthContext';
 import UserAvatar from '../Nav/UserAvatar';
 import authHooks from '../../auth/AuthHooks';
+import { ToastContainer, toast } from 'react-toastify';
 
 export default function ProfileSection() {
-    const { user } = useContext(AuthContext);
+    const { user, accessToken, setUser } = useContext(AuthContext);
     const apiService = authHooks.useApiService();
 
-    const updateEmail = (evt) => {};
+    const updateEmail = (evt) => {
+        evt.preventDefault();
+        const email = evt.target[1].value;
+        if (!email) {
+            toast.error('Enter an email address');
+        } else {
+            apiService(accessToken)
+                .patch('/auth/update_email', { email })
+                .then((res) => {
+                    if (res.status === 200) {
+                        toast.success('Email updated');
+                        setUser({ ...user, email: email });
+                    } else {
+                        toast.error('Failed to update email');
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    toast.error('Failed to update email');
+                });
+        }
+    };
 
     return (
         <div className="flex  mt-3 md:px-[200px] flex-start">
+            <ToastContainer />
             <div className="flex flex-col md:flex-col w-full h-full mx-2 md:pl-10 md:pr-[100px] pt-[56px] pb-[70px]">
                 <div
                     className="flex flex-col md:flex-row"
@@ -24,7 +47,7 @@ export default function ProfileSection() {
                     </div>
 
                     <div className="flex flex-col md:ml-[60px] w-full h-full ">
-                        <form>
+                        <form onSubmit={updateEmail}>
                             <div className="flex flex-col w- h-full mb-8">
                                 <label
                                     htmlFor="name"
@@ -32,7 +55,7 @@ export default function ProfileSection() {
                                     style={{
                                         color: 'gray'
                                     }}>
-                                    Preferred Name
+                                    Full Name
                                 </label>
                                 <input
                                     type="text"
@@ -62,12 +85,15 @@ export default function ProfileSection() {
                                     className="w-full h-10 px-4 mt-2 text-base text-black border border-[#A3AAB2] rounded-lg focus:outline-none focus:border-[#E84E4E]"
                                 />
                             </div>
+
+                            <div className="mt-2" style={{ textAlign: 'right' }}>
+                                <button
+                                    type="submit"
+                                    className=" text-[#19C170]  font-semibold text-base py-4 px-[54px] border-[1px] border-[#1BD47B] rounded-md">
+                                    Update Email
+                                </button>
+                            </div>
                         </form>
-                        <div className="flex flex-col ml-auto mt-2">
-                            <button className=" text-[#19C170]  font-semibold text-base py-4 px-[54px] border-[1px] border-[#1BD47B] rounded-md">
-                                Update Email
-                            </button>
-                        </div>
                     </div>
                 </div>
             </div>
