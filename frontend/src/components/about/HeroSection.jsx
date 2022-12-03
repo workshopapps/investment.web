dd8368d369b8f7cdf883a10e161c1c9c057505fa
Pycomet <alfredemmanuelinyang@gmail.com>
2022-12-03 11:33:06 +0100

    remove spark

--- frontend/src/components/about/HeroSection.jsx
+++ frontend/src/components/about/HeroSection.jsx
@@ -8,7 +8,6 @@ const HeroSection = () => {
             className='flex font-HauoraBold bg-[url("/src/assets/about/hero-image.png")] bg-no-repeat bg-cover pl-4 md:pl-0 justify-center text-center items-center h-[200px] md:h-[360px] drop-shadow-2xl :shadow-gray-500'>
             <div className="flex flex-col mt-2 md:mt-10 text-white gap-[6px]">
                 <h1 className="text-4xl lg:text-6xl font-bold">About Us</h1>
-                <p className="text-lg md:text-3xl mt-5 font-Hauora">The Spark that ignites your dreams</p>
             </div>
         </div>
     );
