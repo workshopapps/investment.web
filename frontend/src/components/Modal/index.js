db6a6beeaefb2da90bc693689e5f7313ac9a49ba
Oladimeji Abdrahman Adebayo <67334984+rahmlad-aramide@users.noreply.github.com>
2022-12-03 04:15:36 +0100

    Updated company-profile card modal z-index

--- frontend/src/components/Modal/index.js
+++ frontend/src/components/Modal/index.js
@@ -7,7 +7,7 @@ const index = ({ children, passedFunc, setPassedFunc }) => {
         <div
             className="w-screen fixed top-0 right-0 h-screen bg-[#00000030] flex justify-center items-center p-4 shadow-2xl"
             onClick={() => setPassedFunc(!passedFunc)}>
-            <div className="bg-white max-w-[379px] w-full max-h-[424px] h-1/2 text-4xl rounded-[10px] p-7">
+            <div className="bg-white max-w-[379px] w-full max-h-[424px] h-1/2 text-4xl rounded-[10px] p-7 z-3">
                 <div className="flex justify-end">
                     <Tippy content={<span className="">Close</span>} placement="bottom">
                         <div
