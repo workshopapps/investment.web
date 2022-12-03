781f46438c440284052eb8ed0ae15b2cb66cd142
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-12-03 09:01:29 +0100

    Update database.py

--- backend/api/database/database.py
+++ backend/api/database/database.py
@@ -15,7 +15,7 @@ DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_size=20, max_overflow=0
+    pool_size=1000, max_overflow=0
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
