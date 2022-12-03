ab2e0d1b275daba960e1e7aeaa53e471d9d5b305
Pycomet <alfredemmanuelinyang@gmail.com>
2022-12-03 13:21:12 +0100

    Feat(FE-40): Complete Implementation/Integration Of Company Page With Backend & Metrics Chart

--- frontend/src/components/CompanyProfile/AnalysisCard.jsx
+++ frontend/src/components/CompanyProfile/AnalysisCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { LineChart, Line, CartesianGrid, XAxis, Tooltip, YAxis } from 'recharts';
+import { AreaChart, Area, CartesianGrid, XAxis, Tooltip, YAxis } from 'recharts';
 
 import DownIcon from '../../assets/landingPage/icons/down.svg';
 import UpIcon from '../../assets/landingPage/icons/up.svg';
@@ -14,8 +14,13 @@ const VisualDataCard = () => {
     const { companyId } = useParams();
     const [state, setState] = React.useState({
         show: false,
+        showRevenue: true,
+        showIncome: true,
+        shwoExpenses: true,
+        showProfit: true,
         value: '2022'
     });
+
     const [data, setData] = React.useState([]);
 
     const getStartDate = () => {
@@ -26,6 +31,11 @@ const VisualDataCard = () => {
 
     const getEndDate = () => {
         return `${state.value}-12-31`;
+    };
+
+    const isNegative = (value) => {
+        const res = Math.sign(value) === -1 ? true : false;
+        return res;
     };
 
     React.useEffect(() => {
@@ -40,16 +50,20 @@ const VisualDataCard = () => {
                     const newRevenue = each['total_revenue'] / 1e9;
                     const newDate = `${each['date']}`.substring(0, 4).trim();
                     const newIncome = each['net_income'] / 1e9;
+                    const grossProfit = each['gross_profit'] / 1e9;
+                    const newExpenses = each['operating_expenses'] / 1e9;
                     console.log(newRevenue);
                     const pos = raw.indexOf(each);
 
                     raw[pos]['total_revenue'] = newRevenue;
                     raw[pos]['date'] = newDate;
                     raw[pos]['net_income'] = newIncome;
-                    raw[pos]['price_index'] = [0, 0.5, 1, 1.5, 2, 2.5];
+                    raw[pos]['gross_profit'] = grossProfit;
+                    raw[pos]['operating_expenses'] = newExpenses;
+                    // raw[pos]['price_index'] = [0, 0.5, 1, 1.5, 2, 4];
                 });
 
-                setData(raw);
+                setData(raw.reverse());
                 console.log(data);
             })
             .catch((err) => console.log(err));
@@ -83,7 +97,9 @@ const VisualDataCard = () => {
                                         key={key}
                                         className="text-gray-700 hover:bg-gray-700 hover:text-white block px-4 py-2"
                                         role="menuitem"
-                                        onClick={() => setState({ show: false, value: each })}
+                                        onClick={() =>
+                                            setState({ ...state, show: false, value: each })
+                                        }
                                         tabIndex="-1"
                                         id="menu-item-1">
                                         {each}
@@ -105,19 +121,48 @@ const VisualDataCard = () => {
             </div>
 
             <div className="h-100 my-10 w-full border border-1px bg-transparent">
-                <LineChart width={900} height={500} data={data}>
-                    <Line
-                        type="monotone"
-                        dataKey="total_revenue"
-                        stroke="#333946"
-                        strokeWidth={2}
-                    />
-                    <Line type="monotone" dataKey="net_income" stroke="#1BD47B" strokeWidth={2} />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="3 3" />
+                <AreaChart width={900} height={500} data={data} stackOffset="expand">
+                    {state.showRevenue && (
+                        <Area
+                            type="monotone"
+                            dataKey="total_revenue"
+                            stroke="#1BD47B"
+                            fill="#E8FBF2"
+                            strokeWidth={2}
+                        />
+                    )}
+                    {state.showIncome && (
+                        <Area
+                            type="monotone"
+                            dataKey="net_income"
+                            stroke="#000000"
+                            fill="#E8FBF2"
+                            strokeWidth={2}
+                        />
+                    )}
+                    {state.shwoExpenses && (
+                        <Area
+                            type="monotone"
+                            dataKey="operating_expenses"
+                            stroke="#ff2c2c"
+                            fill="#E8FBF2"
+                            strokeWidth={2}
+                        />
+                    )}
+                    {state.showProfit && (
+                        <Area
+                            type="monotone"
+                            dataKey="gross_profit"
+                            stroke="#3388ff"
+                            fill="#E8FBF2"
+                            strokeWidth={2}
+                        />
+                    )}
+                    <CartesianGrid stroke="white" strokeDasharray="5 5" />
                     <XAxis dataKey="date" />
                     <YAxis dataKey="total_revenue" />
                     <Tooltip />
-                </LineChart>
+                </AreaChart>
             </div>
 
             <div className="mt-5 md:my-5 flex text-sm md:text-lg font-HauoraBold font-bold flex-row justify-between">
@@ -130,52 +175,106 @@ const VisualDataCard = () => {
                 <>
                     <div className="mt-6 mr-5 flex text-sm md:text-lg font-HauoraBold font-bold flex-row justify-between">
                         <span className="flex flex-row md:w-1/4">
-                            <input className="text-[#1BD47B]" type="checkbox" checked />
+                            <input
+                                className="text-[#1BD47B]"
+                                type="checkbox"
+                                checked={state.showRevenue}
+                                onChange={() =>
+                                    setState({ ...state, showRevenue: !state.showRevenue })
+                                }
+                            />
                             <p className="pl-5">Revenue</p>
                         </span>
                         <p className="text-left pr-10">{`${data[0]['total_revenue'].toFixed(
                             2
                         )}B`}</p>
                         <p className="flex flex-row gap-2">
-                            <img src={greenArrow} alt="win" className="w-[20px] md:w-auto" />
+                            <img
+                                src={isNegative(data[0]['revenue_growth']) ? redArrow : greenArrow}
+                                alt="loss"
+                                className="w-[20px] md:w-auto"
+                            />
                             {`${data[0]['revenue_growth'].toFixed(2)}%`}
                         </p>
                     </div>
                     <div className="mt-6 mr-5 flex text-sm md:text-lg font-HauoraBold font-bold flex-row justify-between">
                         <span className="flex flex-row md:w-1/4">
-                            <input type="checkbox" />
+                            <input
+                                className="text-[#1BD47B]"
+                                type="checkbox"
+                                checked={state.shwoExpenses}
+                                onChange={() =>
+                                    setState({ ...state, shwoExpenses: !state.shwoExpenses })
+                                }
+                            />
                             <p className="pl-5">Operating expenses</p>
                         </span>
-                        <p className="text-left pr-10">{`${(
-                            data[0]['operating_expenses'] / 1e9
-                        ).toFixed(2)}B`}</p>
+                        <p className="text-left pr-10">{`${data[0]['operating_expenses'].toFixed(
+                            2
+                        )}B`}</p>
                         <p className="flex flex-row gap-2">
-                            <img src={greenArrow} alt="win" className="w-[20px] md:w-auto" />
+                            <img
+                                src={
+                                    isNegative(data[0]['operating_expenses_growth'])
+                                        ? redArrow
+                                        : greenArrow
+                                }
+                                alt="loss"
+                                className="w-[20px] md:w-auto"
+                            />
                             {`${data[0]['operating_expenses_growth'].toFixed(2)}%`}
                         </p>
                     </div>
                     <div className="mt-6 mr-5 flex text-sm md:text-lg font-HauoraBold font-bold flex-row justify-between">
                         <span className="flex flex-row md:w-1/4">
-                            <input type="checkbox" />
+                            <input
+                                className="text-[#1BD47B]"
+                                type="checkbox"
+                                checked={state.showProfit}
+                                onChange={() =>
+                                    setState({ ...state, showProfit: !state.showProfit })
+                                }
+                            />
                             <p className="pl-5">Gross Profit</p>
                         </span>
-                        <p className="text-left pr-10">{`${(data[0]['gross_profit'] / 1e9).toFixed(
+                        <p className="text-left pr-10">{`${data[0]['gross_profit'].toFixed(
                             2
                         )}B`}</p>
                         <p className="flex flex-row gap-2">
-                            <img src={greenArrow} alt="loss" className="w-[20px] md:w-auto" />
+                            <img
+                                src={
+                                    isNegative(data[0]['gross_profit_growth'])
+                                        ? redArrow
+                                        : greenArrow
+                                }
+                                alt="loss"
+                                className="w-[20px] md:w-auto"
+                            />
                             {`${data[0]['gross_profit_growth'].toFixed(2)}%`}
                         </p>
                     </div>
                     <div className="my-6 mr-5 flex text-sm md:text-lg font-HauoraBold font-bold flex-row justify-between">
                         <span className="flex flex-row md:w-1/4">
-                            <input type="checkbox" />
+                            <input
+                                className="text-[#1BD47B]"
+                                type="checkbox"
+                                checked={state.showIncome}
+                                onChange={() =>
+                                    setState({ ...state, showIncome: !state.showIncome })
+                                }
+                            />
                             <p className="pl-5">Net Income</p>
                         </span>
-                        <p className="text-left pr-10">-3.49B</p>
+                        <p className="text-left pr-10">{`${data[0]['net_income'].toFixed(2)}B`}</p>
                         <p className="flex flex-row gap-2">
-                            <img src={redArrow} alt="loss" className="w-[20px] md:w-auto" />
-                            9.99%
+                            <img
+                                src={
+                                    isNegative(data[0]['net_income_growth']) ? redArrow : greenArrow
+                                }
+                                alt="loss"
+                                className="w-[20px] md:w-auto"
+                            />
+                            {`${data[0]['net_income_growth'].toFixed(2)}%`}
                         </p>
                     </div>
                 </>
