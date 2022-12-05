86cc01014a43c6c51b8dee4ad37ec46dd236eebd
Nafiu Taiwo <69268373+taiwonaf@users.noreply.github.com>
2022-12-05 12:51:04 +0100

    Added loading component

--- frontend/src/components/watchlist/WatchTable.jsx
+++ frontend/src/components/watchlist/WatchTable.jsx
@@ -2,18 +2,22 @@ import React, { useEffect, useState, useContext } from 'react';
 import StockCard from './StockCard';
 import authHooks from '../../auth/AuthHooks';
 import AuthContext from '../../auth/AuthContext';
+import { ThreeDots } from 'react-loader-spinner';
 
 const WatchTable = ({ onSuccess, onFailure }) => {
     const [watchlist, setWatchlist] = useState([]);
     const { accessToken } = useContext(AuthContext);
+    const [loading, setLoading] = useState(false);
     const apiService = authHooks.useApiService();
 
     const fetchWatchList = () => {
+        setLoading(true);
         if (!accessToken) return;
         apiService(accessToken)
             .get('/user/watchlist')
             .then((res) => {
                 if (res.status === 200) {
+                    setLoading(false);
                     setWatchlist(res.data);
                 }
             })
@@ -24,20 +28,35 @@ const WatchTable = ({ onSuccess, onFailure }) => {
         fetchWatchList();
     }, [accessToken]);
 
+    const watchlistItems = watchlist.map((stock, index) => {
+        return (
+            <StockCard
+                stock={stock}
+                key={index}
+                reload={fetchWatchList}
+                onSuccess={onSuccess}
+                onFailure={onFailure}
+            />
+        );
+    });
+
     return (
         <div>
             <div className="flex justify-around items-center gap-x-[8px] gap-y-[24px] flex-wrap">
-                {watchlist.map((stock, index) => {
-                    return (
-                        <StockCard
-                            stock={stock}
-                            key={index}
-                            reload={fetchWatchList}
-                            onSuccess={onSuccess}
-                            onFailure={onFailure}
+                {loading && (
+                    <div className="h-[50vh] flex justify-center items-center w-full bg-white">
+                        <ThreeDots
+                            height="80"
+                            width="80"
+                            color="#49dd95"
+                            ariaLabel="bars-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                            visible={true}
                         />
-                    );
-                })}
+                    </div>
+                )}
+                {watchlistItems}
             </div>
         </div>
     );
