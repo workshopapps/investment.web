8f214b1014e6cddd8c74f04b2453327fcffe3a55
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-12-05 08:09:56 +0100

    Added notification settings page

--- frontend/src/components/settings/Notificationsettings.jsx
+++ frontend/src/components/settings/Notificationsettings.jsx
@@ -1,19 +1,26 @@
 import { React, useState } from 'react';
-import ReactSwitch from 'react-switch';
+//import ReactSwitch from 'react-switch';
+import toggleOn from '../../assets/notificationSettigns/toggleOn.svg';
+import toggleOff from '../../assets/notificationSettigns/toggleOff.svg';
+// import classNames from 'classnames';
 
 export default function Notificationsettings() {
-    const [checked, setChecked] = useState({
-        switch1: false,
-        switch2: false,
-        switch3: false,
-        switch4: false,
-        switch5: false,
-        switch6: false
-    });
-    // handle switch changes
-    // add max-width: 1028px mx-auto
-    const handleChange = (e) => {
-        setChecked({ ...checked, [e.target.name]: e.target.checked });
+    const [ischecked1, setIsChecked1] = useState(true);
+    const [ischecked2, setIsChecked2] = useState(false);
+    const [ischecked3, setIsChecked3] = useState(true);
+    const [ischecked4, setIsChecked4] = useState(false);
+
+    const handleChecked = () => {
+        setIsChecked1((prev) => !prev);
+    };
+    const handleCheckedTwo = () => {
+        setIsChecked2((prev) => !prev);
+    };
+    const handleCheckedThree = () => {
+        setIsChecked3((prev) => !prev);
+    };
+    const handleCheckedFour = () => {
+        setIsChecked4((prev) => !prev);
     };
 
     return (
@@ -22,19 +29,11 @@ export default function Notificationsettings() {
                 <div className="flex flex-col w-full h-full py-2 font-semibold text-base text-black max-w-[1028px]">
                     <div className="flex justify-between items-center  px-6 py-4">
                         <h2 className="text-xl font-normal">Enable email notification</h2>
-                        <ReactSwitch
-                            checked={checked.switch1}
-                            name="switch1"
-                            onChange={handleChange}
-                            onColor="#19C170"
-                            onHandleColor="#ffffff"
-                            handleDiameter={13}
-                            uncheckedIcon={false}
-                            checkedIcon={false}
-                            height={21}
-                            width={27}
-                            className="ml-4"
-                        />
+                        {ischecked1 ? (
+                            <img src={toggleOff} onClick={handleChecked} alt="toggle" />
+                        ) : (
+                            <img src={toggleOn} onClick={handleChecked} alt="toggle" />
+                        )}
                     </div>
                     <hr className="w-full border" />
                     <div className="flex flex-col w-full h-full py-4 font-semibold text-base text-#0A0B0D ">
@@ -42,116 +41,34 @@ export default function Notificationsettings() {
                             <h2 className="text-l text-#0A0B0D font-normal">
                                 Receive notifications about small cap stocks to invest in{' '}
                             </h2>
-                            <ReactSwitch
-                                checked={checked.switch1}
-                                name="switch1"
-                                onChange={handleChange}
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
+                            {ischecked2 ? (
+                                <img src={toggleOff} onClick={handleCheckedTwo} alt="toggle" />
+                            ) : (
+                                <img src={toggleOn} onClick={handleCheckedTwo} alt="toggle" />
+                            )}
                         </div>
 
                         <div className="flex justify-between items-center px-6 py-6">
                             <h2 className="text-l text-#0A0B0D font-normal">
                                 Receive notifications about large cap stocks to invest in{' '}
                             </h2>
-                            <ReactSwitch
-                                checked={checked.switch2}
-                                onChange={handleChange}
-                                name="switch2"
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
+                            {ischecked3 ? (
+                                <img src={toggleOff} onClick={handleCheckedThree} alt="toggle" />
+                            ) : (
+                                <img src={toggleOn} onClick={handleCheckedThree} alt="toggle" />
+                            )}
                         </div>
 
                         <div className="flex justify-between items-center px-6 py-6">
                             <h2 className="text-l text-#0A0B0D font-normal">
                                 Receive notifications about mid cap stocks to invest in{' '}
                             </h2>
-                            <ReactSwitch
-                                checked={checked.switch3}
-                                onChange={handleChange}
-                                name="switch3"
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
+                            {ischecked4 ? (
+                                <img src={toggleOff} onClick={handleCheckedFour} alt="toggle" />
+                            ) : (
+                                <img src={toggleOn} onClick={handleCheckedFour} alt="toggle" />
+                            )}
                         </div>
-
-                        {/* <div className="flex justify-between items-center py-6">
-                            <h2 className="text-xl font-normal">
-                                Notify me about stocks that should be in my watchlist{' '}
-                            </h2>
-                            <ReactSwitch
-                                checked={checked.switch4}
-                                onChange={handleChange}
-                                name="switch4"
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
-                        </div>
-                        <hr />
-                        <div className="flex justify-between items-center py-6">
-                            <h2 className="text-xl font-normal">
-                                Notify me about investment advice updates{' '}
-                            </h2>
-                            <ReactSwitch
-                                checked={checked.switch5}
-                                onChange={handleChange}
-                                name="switch5"
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
-                        </div>
-                        <hr />
-                        <div className="flex justify-between items-center py-6">
-                            <h2 className="text-xl font-normal">
-                                Send investment notification to my email{' '}
-                            </h2>
-                            <ReactSwitch
-                                checked={checked.switch6}
-                                onChange={handleChange}
-                                name="switch6"
-                                onColor="#19C170"
-                                onHandleColor="#ffffff"
-                                handleDiameter={13}
-                                uncheckedIcon={false}
-                                checkedIcon={false}
-                                height={21}
-                                width={27}
-                                className="ml-4"
-                            />
-                        </div>
-                        <hr /> */}
                     </div>
                     <div className="flex flex-col md:ml-auto mr-4 mt-[70px]">
                         <button className="bg-[#19C170] text-black  font-semibold text-base py-3 px-6 rounded-md">
