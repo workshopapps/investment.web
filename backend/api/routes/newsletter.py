a2809e51343bf4df8249b18adc919b2d798ce4a7
Conradgabe <gisuekebho5880@gmail.com>
2022-12-07 21:30:52 +0000

    newletter function to subscribe users

---
+++ backend/api/routes/newsletter.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Depends
+
+from starlette.requests import Request
+from sqlalchemy.orm import Session
+
+from api.crud.base import get_db
+from api.scripts.email import send_email
+from api.models.models import NewsLetter
+
+router = APIRouter()
+
+
+# subscribed user
+@router.post('/newsletter-subscribe', tags=['EmailSubscription'])
+async def newsletter_subscribed(request: Request, user_email: str, db: Session=Depends(get_db)):
+    if request:
+        users = db.query(NewsLetter).filter(NewsLetter.email == user_email).first()
+        if users is None:
+            insert_news_sub = NewsLetter(email=user_email, subscribed=True)
+            db.add(insert_news_sub)
+            db.commit()
+            db.refresh(insert_news_sub)
+            return {"isnewLetterSubscribed": {"email": insert_news_sub.email, "subscribed": insert_news_sub.subscribed}}
+        else:
+            users.subscribed = True
+            db.add(users)
+            db.flush()
+            db.commit()
+            db.refresh(users)
+            return {"isnewLetterSubscribed": {"email": users.email, "subscribed": users.subscribed}}
+        
+
+# cancel newletter subscription
+@router.post('/newsletter-unsubscribe', tags=['EmailSubscription'])
+async def newsletter_unsubscribed(request: Request, user_email: str, db: Session=Depends(get_db)):
+    if request:
+        users = db.query(NewsLetter).filter(NewsLetter.email == user_email).first()
+        users.subscribed = False
+        db.add(users)
+        db.flush
+        db.commit()
+        db.refresh(users)
+
+        return {"isnewLetterSubscribed": {"email": users.email, "subscribed": users.subscribed}}
+
+# get list of subscribed user and send newsletter email
+async def newsletter_sub_user():
+    users = db.query(NewsLetter).filter(NewsLetter.subscribed == True)
+    newsletter = []
+    for user in users:
+        newsletter.append(user.email)
+    return newsletter
+
+    # test body message
+    body = "<html> <body><h4>Top 12 Recommended Stocks</h4>"
+    body += "<table><thead><tr><th>Company Name</th><th>Ranking Score</th><th>Position</th></tr></thead>"
+    body += "<tbody>"
+    
+    await send_email("NewsLetter", newsletter, body)
