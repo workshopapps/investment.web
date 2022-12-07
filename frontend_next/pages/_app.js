d46180f40f90343453003c0cb6d6e63c21f15b7f
Pycomet <alfredemmanuelinyang@gmail.com>
2022-12-08 00:31:25 +0100

    add viewport

--- frontend_next/pages/_app.js
+++ frontend_next/pages/_app.js
@@ -1,10 +1,14 @@
 import "../styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
-
 import AuthProvider from "../components/auth/AuthProvider";
+import Head from "next/head";
+
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </AuthProvider>
   );
