a03b456fee43a134c44d422ffb848e4c375197e7
Imran Abdulmalik <isoteriksoftware@gmail.com>
2022-12-07 09:25:04 +0100

    configured tailwind

--- frontend_next/tailwind.config.js
+++ frontend_next/tailwind.config.js
@@ -5,7 +5,40 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        HauoraLight: ["Hauora-Light"],
+        Hauora: ["Hauora-Regular"],
+        HauoraBold: ["Hauora-Bold"],
+      },
+      backgroundImage: {
+        "hero-desktop": "url('./assets/index/header-desktop.svg')",
+        "hero-mobile": "url('./assets/index/header-mobile.svg')",
+        "how-mobile": "url('./assets/how-it-works/bg-mobile.png')",
+        "how-desktop": "url('./assets/how-it-works/bg-desktop.png')",
+        "desk-signup": "url('./assets/signup/bg-signup.svg')",
+      },
+      colors: {
+        primary101: "#0B5934",
+        primary102: "#1BD47B",
+        primaryGray: "#525A65",
+        primary103: "#139757",
+        secondaryBlack: "#000511",
+        shade500: "#F5F5F5",
+        shade400: "#F9F9F9",
+        shade100: "#E0E2E5",
+        shadeBlue: "#CCE3FF",
+        primary104: "#1F2226",
+      },
+      width: {
+        210: "300px",
+        520: "520px",
+        95: "95%",
+        295: "295px",
+        460: "460px",
+        1028: "1028px",
+      },
+    },
   },
   plugins: [],
 };
