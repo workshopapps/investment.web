4c18b63e832942039a2ed060f968193faafded0d
Dancyangelo102 <chiomaubaezuonu102@gmail.com>
2022-12-09 20:36:12 +0100

    update

--- frontend/src/pages/contact/Contact.jsx
+++ frontend/src/pages/contact/Contact.jsx
@@ -10,33 +10,41 @@ import ContactForm from './ContactForm';
 const Contact = () => {
     return (
         <PageLayout>
-            <section className="  min-h-fit justify-around p-5 flex md:p-12 flex-col-reverse md:flex-row bg-[#F4F5F6] text-[#525A65]">
+            <section className=" bg-[#FFFFFF] min-h-fit justify-around p-5 flex md:p-12 flex-col-reverse md:flex-row md:bg-[#F4F5F6]">
                 <div className="basis-[30%] font-Hauora p-6 ">
-                    <aside className="mb-4 hidden md:block">
-                        <h1 className="font-bold sm:text-[24px] md:text-5xl p-2">Contact us</h1>
-                        <p className="">Got any concerns you want us to sort out for you?</p>
+                    <aside className="mb-4 hidden md:block py-2">
+                        <h1 className="font-bold pb-2 sm:text-[24px] md:text-[#1F2226] md:text-5xl ">
+                            Contact us
+                        </h1>
+                        <p className="text-[#545964] text-base">
+                            Got any concerns you want us to sort out for you?
+                        </p>
                     </aside>
-                    <aside className="mb-4">
-                        <h2 className="font-light text-xl">For general Enquiries</h2>
-                        <p>support@yieldvest.com</p>
+                    <aside className="mb-4 py-2">
+                        <h2 className="font-normal text-xl md:text-[#1F2226]">
+                            For general Enquiries
+                        </h2>
+                        <p className="text-base">support@yieldvest.com</p>
                     </aside>
-                    <aside className="mb-4">
-                        <h2 className="font-light text-xl">Mailing Address</h2>
-                        <p className="text-[#545964]">
+                    <aside className="mb-4 py-2">
+                        <h2 className="font-normal text-xl md:text-[#1F2226]">Mailing Address</h2>
+                        <p className="text-[#545964] text-base">
                             Plot 1b, industrial avenue, Apapa. Lagos +2348100123456
                             enquiries@yieldvest.com
                         </p>
                     </aside>
-                    <aside className="mb-4">
-                        <h2 className="font-light text-xl">
+                    <aside className="mb-4 py-2">
+                        <h2 className="font-normal text-xl md:text-[#1F2226]">
                             For Media, Business development and partnerships:
                         </h2>
-                        <p className="text-[#545964]">business@yieldvest.com</p>
+                        <p className="text-[#545964] text-base">business@yieldvest.com</p>
                     </aside>
-                    <aside className="mb-4">
-                        <h2 className="font-light text-xl">Follow us on all our social pages</h2>
+                    <aside className="mb-4 py-2">
+                        <h2 className="font-normal text-xl md:text-[#1F2226]">
+                            Follow us on all our social pages
+                        </h2>
 
-                        <aside className="flex mt-6 gap-2">
+                        <aside className="flex mt-3 gap-2">
                             <img src={contactInsta} className="text-3xl w-[40px]" />
                             <img src={contactLinkedIn} className="mx-4 text-3xl w-[36px]" />
                             <img src={contactTwitter} className="text-3xl w-[33px]" />
@@ -46,9 +54,9 @@ const Contact = () => {
                 <div className="basis-[50%] ">
                     <ContactForm />
                 </div>
-                <aside className="mb-4 block md:hidden">
-                    <h1 className=" text-5xl">Contact us</h1>
-                    <p className="text-[#545964]">
+                <aside className="mb-2 block md:hidden">
+                    <h1 className=" text-2xl font-bold">Contact us</h1>
+                    <p className="text-[#525A65]">
                         Got any concerns you want us to sort out for you?
                     </p>
                 </aside>
