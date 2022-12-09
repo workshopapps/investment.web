5137a6ab7edff9fa4b8f1abfb77f82f4f3268000
ayomide <adeshinaayomide12@yahoo.com>
2022-12-09 22:29:57 +0100

    Feature: Enabled Atatus monitoring FE

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import { BrowserTracing } from '@sentry/tracing';
 import reportWebVitals from './reportWebVitals.js';
 import './index.css';
 import App from './App';
+import * as atatus from 'atatus-spa';
+atatus.config('2633350d639c4095ba4d068ad167d5b3').install();
 
 Sentry.init({
     dsn: 'https://2eca6f62796644d4aa995a7fd5c073bd@o4504281377996800.ingest.sentry.io/4504281381601280',
@@ -23,3 +25,4 @@ root.render(
     </React.StrictMode>
 );
 reportWebVitals();
+atatus.notify(new Error('Test Atatus Setup'));
